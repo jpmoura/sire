093cac38e55652130d4a02f2eb79fc7edaa158a6
jpmoura <moura.joaopedro@gmail.com>
2017-01-17 16:14:08 -0200

    Remoção do tratamento errado de usuário NTI no quadro de reservas

--- resources/views/allocation/show.blade.php
+++ resources/views/allocation/show.blade.php
@@ -110,12 +110,12 @@
                                                     @if($alocacao->aula == ($j . $turno) && $alocacao->data == $dias[$k]) {{-- Se for igual então está reservado --}}
 
                                                         {{-- Tratamento para não motrar o e-mail e nome dos administradores --}}
-                                                        @cannot('administrate')
-                                                            @php
-                                                                $alocacao->autorEMAIL = "suporteinformatica@decea.ufop.br";
-                                                                $alocacao->autorNOME = "NTI";
-                                                            @endphp
-                                                        @endcannot
+                                                        {{--@if(Auth::user()->isAdmin())--}}
+                                                            {{--@php--}}
+                                                                {{--$alocacao->autorEMAIL = "suporteinformatica@decea.ufop.br";--}}
+                                                                {{--$alocacao->autorNOME = "NTI";--}}
+                                                            {{--@endphp--}}
+                                                        {{--@endif--}}
 
                                                         <a target="_blank" href="mailto:{{$alocacao->autorEMAIL}}?subject=[UFOP-ICEA] Alocação do recurso {{$recursoNome}}">{{$alocacao->autorNOME}}</a>
 
