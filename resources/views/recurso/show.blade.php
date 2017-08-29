59204ca7a30bf7a6daa0b612c82f4b598f64b58e
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-08-29 16:51:45 -0300

    Ocultação do código da modal de exclusão para quem não pode administrar o sistema

--- resources/views/recurso/show.blade.php
+++ resources/views/recurso/show.blade.php
@@ -127,27 +127,30 @@
         </div><!-- /.col -->
     </div><!-- /.row -->
 
-    <div class="row">
-        <div class="modal fade modal-warning" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModal" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title text-center">Você tem certeza?</h4>
-                    </div>
-                    <div class="modal-body">
-                        <p id="mensagem" class="text-center"></p>
-                    </div>
-                    <div class="modal-footer">
-                        <form class="form" action="{{ route('deleteAsset') }}" method="post">
-                            {{ csrf_field() }}
-                            <input type="hidden" id="formID" name="id" value="">
-                            <button type="button" class="btn btn-default pull-left" data-dismiss="modal"><i class='fa fa-times'></i> Não, quero cancelar</button>
-                            <button type="submit" class="btn btn-success pull-right"><i class='fa fa-check'></i> Sim, quero excluir</button>
-                        </form>
+    @can('administrate')
+        <div class="row">
+            <div class="modal fade modal-warning" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModal" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title text-center">Você tem certeza?</h4>
+                        </div>
+                        <div class="modal-body">
+                            <p id="mensagem" class="text-center"></p>
+                        </div>
+                        <div class="modal-footer">
+                            <form class="form" action="{{ route('deleteAsset') }}" method="post">
+                                {{ csrf_field() }}
+                                <input type="hidden" id="formID" name="id" value="">
+                                <button type="button" class="btn btn-default pull-left" data-dismiss="modal"><i class='fa fa-times'></i> Não, quero cancelar</button>
+                                <button type="submit" class="btn btn-success pull-right"><i class='fa fa-check'></i> Sim, quero excluir</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endcan
+
 @endsection
