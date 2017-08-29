36d78b1a835d50888ba00b9f5ad52f75bf83a170
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-08-29 20:04:08 -0300

    Correção do modelo Regra

--- app/Regra.php
+++ app/Regra.php
@@ -16,7 +16,7 @@ class Regra extends Model
     protected $fillable = [
         'quantidade_horarios_matutino', 'quantidade_horarios_vespertino', 'quantidade_horarios_noturno', 'quantidade_dias_reservaveis',
         'horario_inicio_matutino', 'horario_inicio_vespertino', 'horario_inicio_noturno', 'quantidade_horarios_seguidos',
-        'intervalo_entre_horarios_seguidos'
+        'intervalo_entre_horarios_seguidos', 'tempo_duracao_horario',
     ];
 
     /**
