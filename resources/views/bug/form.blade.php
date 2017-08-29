908d6cafc5766585e7e64f615fb94ae4912d14e3
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-08-29 19:07:35 -0300

    Adição de tooltips nas inputs do formulário de bug

--- resources/views/bug/form.blade.php
+++ resources/views/bug/form.blade.php
@@ -3,7 +3,8 @@
 <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
     <div class="input-group">
         <span class="input-group-addon"><i class="fa fa-tag"></i></span>
-        <input type="text" name="title" maxlength="100" class="form-control" placeholder="Título" required>
+        <input type="text" name="title" maxlength="100" class="form-control" placeholder="Título" data-toggle="tooltip"
+               data-placement="right" title="Título para o bug encontrado." required>
     </div>
     @if ($errors->has('nome'))
         <span class="help-block">
@@ -15,7 +16,10 @@
 <div class="form-group {{ $errors->has('description') ? 'has-error' : '' }}">
     <div class="input-group">
         <span class="input-group-addon"><i class="fa fa-th-list"></i></span>
-        <textarea name="description" maxlength="850" rows="6" style="resize: none" class="form-control" placeholder="Informe o máximo de detalhes que conseguir (Ex.: navegador, ação que estava realizando, se era navagação privada ou não, se era em um dispositivo móvel ou desktop, etc.)" required></textarea>
+        <textarea name="description" maxlength="850" rows="6" style="resize: none" class="form-control"
+                  placeholder="Informe o máximo de detalhes que conseguir (Ex.: navegador, ação que estava realizando, se era navagação privada ou não, se era em um dispositivo móvel ou desktop, etc.)"
+                  data-toggle="tooltip" data-placement="right" title="Descrição detalhada do bug encontrado por você."
+                  required></textarea>
     </div>
     @if ($errors->has('nome'))
         <span class="help-block">
