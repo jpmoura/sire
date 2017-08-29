15eb88f3d59bd6790304f07add494bde69b3dde8
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-08-29 18:50:31 -0300

    Adequação do formulário de envio de bug, usando a classe from-group agora

--- resources/views/bug/form.blade.php
+++ resources/views/bug/form.blade.php
@@ -1,11 +1,26 @@
 {{ csrf_field() }}
 
-<div class="input-group">
-    <span class="input-group-addon"><i class="fa fa-tag"></i></span>
-    <input type="text" name="title" maxlength="100" class="form-control" placeholder="Título" required>
+<div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
+    <div class="input-group">
+        <span class="input-group-addon"><i class="fa fa-tag"></i></span>
+        <input type="text" name="title" maxlength="100" class="form-control" placeholder="Título" required>
+    </div>
+    @if ($errors->has('nome'))
+        <span class="help-block">
+            <strong>{{ $errors->first('title') }}</strong>
+        </span>
+    @endif
 </div>
 
-<div class="input-group">
-    <span class="input-group-addon"><i class="fa fa-th-list"></i></span>
-    <textarea name="description" maxlength="850" rows="6" style="resize: none" class="form-control" placeholder="Informe o máximo de detalhes que conseguir (Ex.: navegador, ação que estava realizando, se era navagação privada ou não, se era em um dispositivo móvel ou desktop, etc.)" required></textarea>
+<div class="form-group {{ $errors->has('description') ? 'has-error' : '' }}">
+    <div class="input-group">
+        <span class="input-group-addon"><i class="fa fa-th-list"></i></span>
+        <textarea name="description" maxlength="850" rows="6" style="resize: none" class="form-control" placeholder="Informe o máximo de detalhes que conseguir (Ex.: navegador, ação que estava realizando, se era navagação privada ou não, se era em um dispositivo móvel ou desktop, etc.)" required></textarea>
+    </div>
+    @if ($errors->has('nome'))
+        <span class="help-block">
+            <strong>{{ $errors->first('description') }}</strong>
+        </span>
+    @endif
 </div>
+
