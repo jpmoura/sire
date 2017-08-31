992ddbdad1494d13eb294a53883f3ae337730b4c
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-08-31 00:27:31 -0300

    Adicionado as factories de criação do modelo Usuário

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,30 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Usuario::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
+        'nome' => $faker->name,
+        'cpf' => $faker->cpf,
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
+});
+
+$factory->defineAs(App\Usuario::class, 'admin', function ($faker) use ($factory) {
+    $user = $factory->raw(App\Usuario::class);
+
+    return array_merge($user, ['nivel' => 1]);
+});
+
+$factory->defineAs(App\Usuario::class, 'normal', function ($faker) use ($factory) {
+    $user = $factory->raw(App\Usuario::class);
+
+    return array_merge($user, ['nivel' => 2]);
+});
+
+$factory->defineAs(App\Usuario::class, 'especial', function ($faker) use ($factory) {
+    $user = $factory->raw(App\Usuario::class);
+
+    return array_merge($user, ['nivel' => 3]);
 });
