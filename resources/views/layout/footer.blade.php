4970e701882350597fcc45c5ff86c5b4aa2ea8b9
Joao Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-12-24 14:25:02 -0200

    Mudança na geração da rota da página sobre o sistema

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,16 +2,16 @@
     <footer class="main-footer">
         <!-- To the right -->
         <div class="pull-right hidden-xs">
-            <a href="{{url('/sobre#modificacoes')}}">{{ config('app.version') }}</a>
+            <a href="{{ route('about.show') . "#modificacoes" }}">{{ config('app.version') }}</a>
         </div>
         <!-- Default to the left -->
-        Copyleft <i class="fa fa-creative-commons"></i> {{ date("Y") }} <a href="{{url('/sobre#')}}"><strong>João Pedro Santos de Moura</strong></a>.
+        Copyleft <i class="fa fa-creative-commons"></i> {{ date("Y") }} <a href="{{ route('about.show') }}"><strong>João Pedro Santos de Moura</strong></a>.
     </footer>
 @else
     <footer>
         <div class="col-md-8 col-md-offset-2 text-center">
             <hr>
-            Copyleft <i class="fa fa-creative-commons"></i> {{ date("Y") }} <a href="{{url('/sobre#')}}"><strong>João Pedro Santos de Moura</strong></a>.
+            Copyleft <i class="fa fa-creative-commons"></i> {{ date("Y") }} <a href="{{ route('about.show') . '#modificacoes' }}"><strong>João Pedro Santos de Moura</strong></a>.
             <div class="pull-right hidden-xs">
                 <a href="{{url('/sobre#modificacoes')}}">{{ config('app.version') }}</a>
             </div>
