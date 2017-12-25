a82a39f992225839e25c17eda051a9ebbdfbc35f
Joao Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-12-25 15:51:01 -0200

    Adição do escopos locais para Reserva

--- app/Reserva.php
+++ app/Reserva.php
@@ -31,4 +31,34 @@ class Reserva extends Model
     {
         return $this->belongsTo('App\Recurso');
     }
+
+    /**
+     * @param Query $query Query a ser modificada
+     * @param \DateTime $diaInicial Data a partir que as reservas serão procuradas
+     * @return mixed Query extendida
+     */
+    public function scopeIniciandoEm($query, $diaInicial)
+    {
+        return $query->where('data', '>=', $diaInicial);
+    }
+
+    /**
+     * @param $query Query a ser modificada
+     * @param \DateTime $diaFinal Dia limite o qual as reservas serão recuperadas
+     * @return mixed Query extendida
+     */
+    public function scopeFinalizandoEm($query, $diaFinal)
+    {
+        return $query->where('data', '<=', $diaFinal);
+    }
+
+    /**
+     * @param $query Query a ser modificada
+     * @param \DateTime $dataEspecifica Data a qual as reservas serão procuradas
+     * @return mixed Query extendida
+     */
+    public function scopeEm($query, $dataEspecifica)
+    {
+        return $query->where('data', $dataEspecifica);
+    }
 }
